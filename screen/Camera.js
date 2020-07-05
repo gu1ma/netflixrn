ad0e0f0255b77d04176915e402a7ef04ca3b625f
Gabriel Guimarães <gabrielgs1408@gmail.com>
2020-07-05 16:15:21 -0300

    init: commit inicial

---
+++ screen/Camera.js
@@ -0,0 +1,9 @@
+'use strict';
+import React, {PureComponent} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
+export default class Camera extends PureComponent {
+  render() {
+    return <View></View>;
+  }
+}
